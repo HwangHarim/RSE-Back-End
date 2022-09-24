8dd0603cd4ea62478edbb39cf06f2775edec63ed
HwangHarim <scopan01@naver.com>
2022-09-24 13:35:16 +0900

    Setting - rse

---
+++ src/main/java/com/game/rse/RseApplication.java
@@ -0,0 +1,13 @@
+package com.game.rse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/game/rse/RseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.game.rse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
