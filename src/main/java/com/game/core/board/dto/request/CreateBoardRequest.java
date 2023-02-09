8ffb2cc275eb8f9865e73613021aacf4c6639047
HwangHarim <scopan01@naver.com>
2023-02-09 23:51:38 +0900

    FIX : Board 수정

--- src/main/java/com/game/core/board/dto/request/CreateBoardRequest.java
+++ src/main/java/com/game/core/board/dto/request/CreateBoardRequest.java
@@ -1,25 +1,18 @@
 package com.game.core.board.dto.request;
 
-import com.game.core.board.domain.vo.LikeTag;
-import com.game.core.board.domain.vo.Type;
 import java.time.LocalDateTime;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 @Data
 @Builder
-@AllArgsConstructor
-@NoArgsConstructor
 public class CreateBoardRequest {
     private Long id;
-    private String nickName;
+    private String userName;
     private String title;
     private String content;
-    private Type type;
+    private String type;
     private LocalDateTime createTime;
     private LocalDateTime modified;
-    private LikeTag like;
     private Long views;
 }
