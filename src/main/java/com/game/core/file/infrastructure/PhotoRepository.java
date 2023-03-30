3c13ee6b60a26aafddf4bce958537e764fe3b2c8
HwangHarim <scopan01@naver.com>
2023-03-30 23:55:02 +0900

    :art: Refactor: 이미지 디렉토리 구조 변경

--- src/main/java/com/game/core/file/infrastructure/PhotoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.game.core.file.infrastructure;
-
-import com.game.core.file.domain.Photo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PhotoRepository extends JpaRepository<Photo, Long> {
-}
